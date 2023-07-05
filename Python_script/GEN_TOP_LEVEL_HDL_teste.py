e331910ea02aae2dda1ae72e1c5db18a707e8574
Luis Antonio Spader Simon <luisspaders@gmail.com>
2023-07-05 18:32:28 -0300

    clean NN folders

--- Python_script/GEN_TOP_LEVEL_HDL_teste.py
+++ Python_script/GEN_TOP_LEVEL_HDL_teste.py
@@ -1,12 +1,11 @@
 from GEN_TOP_LEVEL_HDL import *
 
-INPUTS_NUMBER = 64
+INPUTS_NUMBER = 10
 BIT_WIDTH = 8
 # LAYER_NEURONS_NUMBER_LIST = [4, 3, 2, 3, 4, 64]
 # LAYER_NEURONS_NUMBER_LIST = [15, 11, 7, 3, 2]
 # LAYER_NEURONS_NUMBER_LIST = [16, 12, 8, 4, 2]
 # LAYER_NEURONS_NUMBER_LIST = [10, 10]
-LAYER_NEURONS_NUMBER_LIST = [20, 20]
 # LAYER_NEURONS_NUMBER_LIST = [30, 30]
 # LAYER_NEURONS_NUMBER_LIST = [40, 40]
 # LAYER_NEURONS_NUMBER_LIST = [50, 50]
@@ -35,18 +34,32 @@ BARRIERS = False
 BASE_DICT_HIDDEN['Neuron_arch']['Barriers'] = BARRIERS
 BASE_DICT_SOFTMAX['Neuron_arch']['Barriers'] = BARRIERS
 
-# GEN_TOP_LEVEL_HDL(INPUTS_NUMBER=INPUTS_NUMBER,
-#                     BIT_WIDTH=BIT_WIDTH,
-#                     IO_TYPE_STR=IO_TYPE_STR,
-#                     LAYER_NEURONS_NUMBER_LIST=LAYER_NEURONS_NUMBER_LIST,
-#                     BASE_DICT_HIDDEN=BASE_DICT_HIDDEN,
-#                     BASE_DICT_SOFTMAX=BASE_DICT_SOFTMAX,
-#                     OUTPUT_BASE_DIR_PATH=OUTPUT_BASE_DIR_PATH,
-#                     INCLUDE_PARAMETERS_ON_FOLDERNAME=INCLUDE_PARAMETERS_ON_FOLDERNAME,
-#                     DOWNLOAD_VHD=DOWNLOAD_VHD,
-#                     DEAD_NEURONS=DEAD_NEURONS,
-#                     DEBUG=False
-#                     )
+OUTPUT_BASE_DIR_PATH = './NEURONS'
+
+LAYER_NEURONS_NUMBER_LIST = [10, 10, 10, 10, 10, 10, 10]
+# LAYER_NEURONS_NUMBER_LIST = [20, 20, 20, 20, 20, 20, 20]
+# LAYER_NEURONS_NUMBER_LIST = [30, 30, 30, 30, 30, 30, 30]
+# LAYER_NEURONS_NUMBER_LIST = [40, 40, 40, 40, 40, 40, 40]
+# LAYER_NEURONS_NUMBER_LIST = [50, 50, 50, 50, 50, 50, 50]
+# LAYER_NEURONS_NUMBER_LIST = [60, 60, 60, 60, 60, 60, 60]
+# LAYER_NEURONS_NUMBER_LIST = [70, 70, 70, 70, 70, 70, 70]
+# LAYER_NEURONS_NUMBER_LIST = [80, 80, 80, 80, 80, 80, 80]
+# LAYER_NEURONS_NUMBER_LIST = [90, 90, 90, 90, 90, 90, 90]
+# LAYER_NEURONS_NUMBER_LIST = [100, 100, 100, 100, 100, 100, 100]
+
+
+GEN_TOP_LEVEL_HDL(INPUTS_NUMBER=INPUTS_NUMBER,
+                  BIT_WIDTH=BIT_WIDTH,
+                  IO_TYPE_STR=IO_TYPE_STR,
+                  LAYER_NEURONS_NUMBER_LIST=LAYER_NEURONS_NUMBER_LIST,
+                  BASE_DICT_HIDDEN=BASE_DICT_HIDDEN,
+                  BASE_DICT_SOFTMAX=BASE_DICT_SOFTMAX,
+                  OUTPUT_BASE_DIR_PATH=OUTPUT_BASE_DIR_PATH,
+                  INCLUDE_PARAMETERS_ON_FOLDERNAME=INCLUDE_PARAMETERS_ON_FOLDERNAME,
+                  DOWNLOAD_VHD=DOWNLOAD_VHD,
+                  DEAD_NEURONS=DEAD_NEURONS,
+                  DEBUG=False
+                  )
 
 # #  ---------------------------------- bits ----------------------------------
 # bits = [6, 7, 8, 9, 10, 11, 12]
@@ -127,37 +140,37 @@ BASE_DICT_SOFTMAX['Neuron_arch']['Barriers'] = BARRIERS
 #                           )
 
 #  ---------------------------------- NEURONS ----------------------------------
-layers_list = [
-    [10, 10, 10, 10, 10],
-    [20, 20, 20, 20, 20],
-    [30, 30, 30, 30, 30],
-    [40, 40, 40, 40, 40],
-    [50, 50, 50, 50, 50],
-    [60, 60, 60, 60, 60],
-    [70, 70, 70, 70, 70],
-    [80, 80, 80, 80, 80],
-    [90, 90, 90, 90, 90],
-    [100, 100, 100, 100, 100]
-]
+# layers_list = [
+#     [10, 10, 10, 10, 10],
+#     [20, 20, 20, 20, 20],
+#     [30, 30, 30, 30, 30],
+#     [40, 40, 40, 40, 40],
+#     [50, 50, 50, 50, 50],
+#     [60, 60, 60, 60, 60],
+#     [70, 70, 70, 70, 70],
+#     [80, 80, 80, 80, 80],
+#     [90, 90, 90, 90, 90],
+#     [100, 100, 100, 100, 100]
+# ]
 
-for layers in layers_list:
-    for barriers in [True, False]:
+# for layers in layers_list:
+#     for barriers in [True, False]:
 
-        BASE_DICT_HIDDEN['Neuron_arch']['Barriers'] = barriers
-        BASE_DICT_SOFTMAX['Neuron_arch']['Barriers'] = barriers
+#         BASE_DICT_HIDDEN['Neuron_arch']['Barriers'] = barriers
+#         BASE_DICT_SOFTMAX['Neuron_arch']['Barriers'] = barriers
 
-        GEN_TOP_LEVEL_HDL(INPUTS_NUMBER=INPUTS_NUMBER,
-                          BIT_WIDTH=BIT_WIDTH,
-                          IO_TYPE_STR=IO_TYPE_STR,
-                          LAYER_NEURONS_NUMBER_LIST=layers,
-                          BASE_DICT_HIDDEN=BASE_DICT_HIDDEN,
-                          BASE_DICT_SOFTMAX=BASE_DICT_SOFTMAX,
-                          OUTPUT_BASE_DIR_PATH=OUTPUT_BASE_DIR_PATH,
-                          INCLUDE_PARAMETERS_ON_FOLDERNAME=INCLUDE_PARAMETERS_ON_FOLDERNAME,
-                          DOWNLOAD_VHD=DOWNLOAD_VHD,
-                          DEAD_NEURONS=DEAD_NEURONS,
-                          DEBUG=False
-                          )
+#         GEN_TOP_LEVEL_HDL(INPUTS_NUMBER=INPUTS_NUMBER,
+#                           BIT_WIDTH=BIT_WIDTH,
+#                           IO_TYPE_STR=IO_TYPE_STR,
+#                           LAYER_NEURONS_NUMBER_LIST=layers,
+#                           BASE_DICT_HIDDEN=BASE_DICT_HIDDEN,
+#                           BASE_DICT_SOFTMAX=BASE_DICT_SOFTMAX,
+#                           OUTPUT_BASE_DIR_PATH=OUTPUT_BASE_DIR_PATH,
+#                           INCLUDE_PARAMETERS_ON_FOLDERNAME=INCLUDE_PARAMETERS_ON_FOLDERNAME,
+#                           DOWNLOAD_VHD=DOWNLOAD_VHD,
+#                           DEAD_NEURONS=DEAD_NEURONS,
+#                           DEBUG=False
+#                           )
 
 
 print("====================== FIM =========================")
