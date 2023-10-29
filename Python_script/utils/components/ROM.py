b6cf8b51bf3487186352b4a984ee71967ab048ee
Luis Antonio Spader Simon <luisspaders@gmail.com>
2023-10-29 18:12:11 -0300

    remove imports from ROM.py

--- Python_script/utils/components/ROM.py
+++ Python_script/utils/components/ROM.py
@@ -1,8 +1,8 @@
 
-import plotly.offline as py
-import plotly.graph_objects as go
+# import plotly.offline as py
+# import plotly.graph_objects as go
 # import seaborn as sns
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from fxpmath import Fxp
 import math
 import pandas as pd
